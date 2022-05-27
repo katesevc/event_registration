29a08e3cd4f491ca04ad6dcc804ed78eb87bb2e4
BenHbrt <benherbert1983@googlemail.com>
2022-05-27 11:52:36 +0200

    Fixed issue with Event class -waiting for document

--- src/js/createWidgets.js
+++ src/js/createWidgets.js
@@ -1,9 +1,10 @@
 // Makes widgets using the data
 
-import { Event } from './events.js';
+// import { Event } from './events.js';
 
 export const createWidgets = (data) => {
     data.forEach((item) => {
-        new Event(item.id, item.name, item.description, item.image_url, item.date);
+        // new Event(item.id, item.name, item.description, item.image_url, item.date);
+        console.log(item);
     });
 };

3afdea7c82d0f05e0d3f57aa69f0abad11bb2f79
BenHbrt <benherbert1983@googlemail.com>
2022-05-27 11:30:26 +0200

    loading_data

---
+++ src/js/createWidgets.js
@@ -0,0 +1,9 @@
+// Makes widgets using the data
+
+import { Event } from './events.js';
+
+export const createWidgets = (data) => {
+    data.forEach((item) => {
+        new Event(item.id, item.name, item.description, item.image_url, item.date);
+    });
+};
---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// Importing functions
+import { loadData } from './loadData.js';
+import { createWidgets } from './createWidgets.js';
+
+// Importing values
+import { APIkey } from './constants.js'
+
+
+// Takes the data and makes the widgets
+const loadAndCreateWidgets = async () => {
+    const data = await loadData();
+    createWidgets(data);
+}
